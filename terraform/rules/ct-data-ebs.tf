4f6626e74672fc6f82abb64c47de9a0fce671b75
Tim Orr <timorr@RES18LT298.local>
2020-07-05 09:08:56 -0400

    ebs volume permissions

---
+++ terraform/rules/ct-data-ebs.tf
@@ -0,0 +1,20 @@
+module "rule-data-ebs" {
+  source = "../modules/rules"
+
+  name          = "data-ebs"
+  description   = "Capture EBS configuration changes"
+  is_enabled    = true
+  event_pattern = <<-PATTERN
+  {
+    "detail": {
+      "eventSource": [
+        "ec2.amazonaws.com"
+      ],
+      "eventName": [
+        "ModifySnapshotAttribute"
+      ]
+    }
+  }
+  PATTERN
+  sns_topic_arn = var.sns_topic_arn
+}
